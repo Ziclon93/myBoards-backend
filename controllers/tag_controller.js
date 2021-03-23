ac8cb465fd3676438f0ac9fb3e50d0dda702c893
Ziclon93 <ziclon93lh@gmail.com>
2021-03-24 00:47:23 +0100

    tag and post relation

--- controllers/tag_controller.js
+++ controllers/tag_controller.js
@@ -4,15 +4,14 @@ var tagModel = require('../models/tag');
 var boardTagModel = require('../models/board_tag');
 var DataTypes = require('sequelize/lib/data-types');
 
-exports.getTag = function(t_name){
+exports.getTagByName = function(t_name){
     return new Promise(function(resolve, reject){
-        var TagModel = tagModel(sequelize, DataTypes);
-        TagModel.findOne({ where : { tagName: t_name} })
-         .then(function(data){
-           resolve(data);
-         }, function(err){
-           reject(err);
-         });
+        var tag = getTag(t_name);
+        if(tag){
+            resolve(tag);
+        }else{
+            reject();
+        }
     });
 };
 
@@ -31,18 +30,23 @@ exports.postTag = function(t_name){
 };
 
 exports.postTagOfBoard = function(b_id, t_name){
-    
     return new Promise(function(resolve,reject) {
         var BoardTagModel = boardTagModel(sequelize, DataTypes);
         var success = true;
-        this.getTag(t_name).then(tag =>{
-            BoardTagModel.create({
-                tagId : tag.id,
-                boardId : b_id,
-            }).then(boardTag => {
-                console.log("Board-Tag created");
-            }); 
-        });
+        var tag = getTag(t_name)
+        BoardTagModel.create({
+            tagId : tag.id,
+            boardId : b_id,
+        }); 
         resolve(success);
     });
+}
+
+getTag =  function(t_name){
+    var TagModel = tagModel(sequelize, DataTypes);
+    TagModel.findOne({ where : { tagName: t_name} })
+     .then(function(data){
+       return data;
+     });
+    return null;
 }
