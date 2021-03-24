7000d085d853c7a0bc83ec36e1b5c1b386b7a716
Ziclon93 <ziclon93lh@gmail.com>
2021-03-24 02:14:07 +0100

    tag and post relation

--- controllers/tag_controller.js
+++ controllers/tag_controller.js
@@ -20,7 +20,7 @@ exports.postTagOfBoard = function(b_id, t_name){
     return new Promise(function(resolve,reject) {
         var BoardTagModel = boardTagModel(sequelize, DataTypes);
         var success = true;
-        findOrCreateTag(t_name).then(tag =>{
+        findOrCreateTag(t_name).then( tag => {
             console.log("__________________");
             console.log("tag.id" + tag.id);
             console.log("tag" + tag);
@@ -42,6 +42,6 @@ exports.postTagOfBoard = function(b_id, t_name){
 
 findOrCreateTag = function(t_name){
     var TagModel = tagModel(sequelize, DataTypes);
-    var tag = TagModel.findOrCreate({ where : { tagName: t_name} })
+    var tag = await TagModel.findOrCreate({ where : { tagName: t_name} })
     return tag;    
 }
