082edcd9fb790d010ac070b0acee911801820132
Ziclon93 <ziclon93lh@gmail.com>
2021-03-25 01:22:58 +0100

    Posts follows and find boards by follower

--- controllers/follow_controller.js
+++ controllers/follow_controller.js
@@ -11,26 +11,29 @@ exports.postFollow = function(user, f_name){
 
         var success = true;
         var u_id = user['id'];
-        let f_exist = FollowModel.findOne({ where : { followerId: u_id, followedId: f_id } })
+        ctl_user.getUserByUsername(f_name).then( followed =>{
+            var f_id = followed['id'];
+            let f_exist = FollowModel.findOne({ where : { followerId: u_id, followedId: f_id } })
 
-        console.log("_______________22")
-        if(f_exist){
-            console.log("_______________2")
-            resolve(!success)
-        }else{
-            console.log("_______________")
-            FollowModel.create({
-                followerId: u_id,
-                followedId: followed['id']
-            }).then(follow => {
-                console.log("Follow created");
-            }, function (err) {
-                console.log("Error ocurred: " + err);
-                reject(err);
-            }); 
-            console.log("_______________")
-            resolve(success);
-        }
+            console.log("_______________22")
+            if(f_exist){
+                console.log("_______________2")
+                resolve(!success)
+            }else{
+                console.log("_______________")
+                FollowModel.create({
+                    followerId: u_id,
+                    followedId: f_id
+                }).then(follow => {
+                    console.log("Follow created");
+                }, function (err) {
+                    console.log("Error ocurred: " + err);
+                    reject(err);
+                }); 
+                console.log("_______________")
+                resolve(success);
+            }
+        });
     });
 }
 
