f201d2cc2ee5be47bfc2caae0847f4878e39afab
Ziclon93 <ziclon93lh@gmail.com>
2021-05-30 19:11:31 +0200

    Manage get Valoration without boardId in tags model

--- controllers/valoration_controller.js
+++ controllers/valoration_controller.js
@@ -78,7 +78,7 @@ exports.getPostValoration = function(post){
 
         LikeModel.count({where:{postId: post.id}}).then(postLikes =>{
             DislikeModel.count({where:{postId: post.id}}).then(postDislikes =>{
-                resolve((postLikes - postDislikes)/100);
+                resolve((parseFloat(postLikes) - parseFloat(postDislikes))/100);
             },function(err){
                 reject(err);
             })
