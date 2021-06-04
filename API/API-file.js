d0a45d94da3771bb0820200846dcc487fb46055b
Ziclon93 <ziclon93lh@gmail.com>
2021-06-05 00:13:11 +0200

    fixing post a post

--- API/API-file.js
+++ API/API-file.js
@@ -349,8 +349,6 @@ function getBoardData(board) {
             Promise.all(postValorationPromises).then(valorations => {
                 var postList = [];
                 valorations.forEach((tuple, index) => {
-
-                    console.log("_______________________________________-1" + promisesResults[2][index].id);
                     postList.push(
                         {
                             id: promisesResults[2][index].id,
