5066744f21c1dc8216c8e9404e05b24529436602
Ziclon93 <ziclon93lh@gmail.com>
2021-06-20 11:56:45 +0200

    testing

--- controllers/tag_controller.js
+++ controllers/tag_controller.js
@@ -57,7 +57,12 @@ exports.getMostUsedTags = function () {
 
     return new Promise(function (resolve, reject) {
         var BoardTagModel = boardTagModel(sequelize, DataTypes);
-        BoardTagModel.findAll({ attributes: [sequelize.fn('MAX', sequelize.col('tagId'))],group= ['BoardTagModel.boardId'], raw: true, order: sequelize.literal('count DESC')}).then(list => {
+        BoardTagModel.findAll({
+            attributes: [sequelize.fn('MAX', sequelize.col('tagId'))],
+            group=['BoardTagModel.boardId'],
+            raw: true,
+            order: sequelize.literal('count DESC'),
+        }).then(list => {
             list.forEach(result => {
                 console.log("____________________________");
                 console.log(result);
